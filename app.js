0173e594252003bf5dcdf6603c729c6ddccdad42
kakaka820 <tata_ta@163.com>
2025-05-25 13:45:51 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 function login() {
+  console.log("ログイン処理");
+  // 後で本当のログイン処理をここに書く
+}
+function showRegister() {
+  console.log("新規登録表示");
+}
+function register() {
+  console.log("登録処理");
+}
+function backToLogin() {
+  console.log("ログイン画面に戻る");
 }
 
-// グローバルに公開（HTMLのonclickから呼べるように）
+// グローバルに公開（HTML側の onclick から使えるように）
 window.login = login;
-
+window.showRegister = showRegister;
+window.register = register;
+window.backToLogin = backToLogin;
